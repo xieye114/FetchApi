ad32ac81b13cedfb572d5937c1601f047bf834a7
yyy <zhangsanx123@aol.com>
2019-12-31 10:49:37 +0700

    1

---
+++ src/config/fetchapi-pay.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+
+
+    'api_pay_daikou_url' => env( 'API_PAY_DAIKOU_URL'),
+    'api_pay_daikou_org_no' => env( 'API_PAY_DAIKOU_ORGNO'),
+    'api_pay_daikou_mer_no' => env( 'API_PAY_DAIKOU_MERNO'),
+    'api_pay_daikou_md5_key' => env( 'API_PAY_DAIKOU_MD5KEY'),
+    'api_pay_daikou_settle_rate' => env( 'API_PAY_DAIKOU_SETTLE_RATE'),
+    'api_pay_daikou_settle_charge' => env( 'API_PAY_DAIKOU_SETTLE_CHARGE'),
+
+);
